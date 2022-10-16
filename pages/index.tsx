b5760a8fff9b73fa78558e8667b1fab05c5d5a84
xoma_star <sasha.sirotyin09@gmail.com>
2022-10-16 21:31:06 +0500

    release

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
             <h1 className={'text-7xl font-bold px-4 bg-clip-text text-transparent bg-gradient-to-br from-[#7303c0] via-[#ec38bc] to-[#fdeff9] animate-text-shimmer'}>XOMA VPN</h1>
             <h3 className={'text-xl'}><MorphicText words={['надежный', 'удобный', 'быстрый', 'безопасный', 'легкий', 'приватный']}/> VPN сервис<br/>от Александра Сиротина</h3>
         </div>
-        <button className={'absolute bottom-12 text-white border-4 border-white text-xl px-4 py-2'}>
+        <button className={'absolute bottom-12 text-white border-4 border-white text-xl px-4 py-2'} onClick={() => window.open('https://t.me/xomavpn_bot')}>
             Попробовать
         </button>
     </div>
