b8f70a8dc2d8f088127b70316a6b301aa98a33ec
xoma_star <sasha.sirotyin09@gmail.com>
2022-10-17 22:01:39 +0500

    release

--- components/Title/index.tsx
+++ components/Title/index.tsx
@@ -8,8 +8,8 @@ interface props{
 
 const Title = ({size, children, className}: props) => {
     if(size === 's') return <h3 className={'text-2xl font-semibold ' + (className || '')}>{children}</h3>
-    if(size === 'm') return <h3 className={'text-4xl font-bold ' + (className || '')}>{children}</h3>
-    if(size === 'l') return <h3 className={'text-4xl font-bold ' + (className || '')}>{children}</h3>
+    if(size === 'm') return <h3 className={'text-3xl md:text-6xl font-bold ' + (className || '')}>{children}</h3>
+    if(size === 'l') return <h3 className={'text-4xl md:text-8xl font-bold ' + (className || '')}>{children}</h3>
     return <h3 className={className}>{children}</h3>
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,60 @@
 import type { NextPage } from 'next'
 import Head from "next/head";
 import MorphicText from "../components/MorphicText";
+import Image from "next/image";
+import Title from "../components/Title";
 
 const Home: NextPage = () => {
     return <div className={'flex flex-col items-center'}>
         <Head>
             <title>XOMA VPN</title>
         </Head>
-        <div className={'bg-black text-white text-center pb-8 w-full h-[100vh] flex flex-col justify-center relative items-center gap-4'}>
-            <h1 className={'text-7xl font-bold px-4 bg-clip-text text-transparent bg-[url("../public/onni-anttoora-5K8RnvDKBr4-unsplash.jpg")]'} style={{backgroundPosition: '100% 21%', backgroundSize: '100%'}}>
+        <div className={'bg-black text-white text-center pb-8 w-full h-[100vh] flex flex-col justify-center relative items-center gap-4 relative'}>
+            <h1 className={'text-7xl md:text-9xl font-bold px-4 bg-clip-text text-transparent bg-[url("../public/onni-anttoora-5K8RnvDKBr4-unsplash.jpg")]'} style={{backgroundPosition: '100% 21%', backgroundSize: '100%'}}>
                 XOMA VPN
             </h1>
             <h3 className={'text-xl'}><MorphicText words={['надежный', 'удобный', 'быстрый', 'безопасный', 'легкий', 'приватный']}/> VPN сервис<br/>от Александра Сиротина</h3>
+            <button
+                className={'text-white border-4 border-white text-xl px-4 py-2 mt-12 hover:bg-white hover:text-black duration-300'}
+                onClick={() => window.open('https://t.me/xomavpn_bot')}>
+                Попробовать
+            </button>
+            <div className={'absolute bottom-3'}><Image src={'/arrow-down.svg'} width={30} height={30}/></div>
         </div>
-        <button className={'absolute bottom-12 text-white border-4 border-white text-xl px-4 py-2'} onClick={() => window.open('https://t.me/xomavpn_bot')}>
-            Попробовать
-        </button>
+        <div className={'px-8'}>
+            <div className={'py-16 text-justify md:text-left'}>
+                <Title className={'py-8'} size={'l'}>Всего 80 р./мес.</Title>
+                <p className={'md:text-5xl text-3xl'}>Платите 80 рублей раз в месяц. Больше никаких дополнительных сборов и комиссий.</p>
+            </div>
+            <div className={'py-16 text-justify md:text-right'}>
+                <Title className={'py-8'} size={'l'}>Сервера за границей</Title>
+                <p className={'md:text-5xl text-3xl'}>Используйте заблокированные сервисы как прежде. Instagram, Twitter, Facebook, LinkedIn</p>
+            </div>
+            <div className={'py-16 text-justify md:text-left'}>
+                <Title className={'py-8'} size={'l'}>Гигабитный порт</Title>
+                <p className={'md:text-5xl text-3xl'}>Высокая скорость и стабильность соединения. Смотрите стримы без провисаний. Загрузки файлов без ограничей. Что уж говорить о простом серфинге.</p>
+            </div>
+            <div className={'py-16 text-justify md:text-right'}>
+                <Title className={'py-8'} size={'l'}>Скройтесь от посторонних глаз</Title>
+                <p className={'md:text-5xl text-3xl'}>Трафик, проходящий через VPN шифруется, а ваш IP недоступен внешним сайтам. Мошенникам будет сложнее вас отследить, а ваши данные будут в безопасности.</p>
+            </div>
+            <div className={'py-16 text-justify md:text-left'}>
+                <Title className={'py-8'} size={'l'}>Безопасный Интернет за пару секунд</Title>
+                <p className={'md:text-5xl text-3xl'}>Подключитесь к VPN нажатием на один переключатель и забудьте о нем. XOMA VPN сам сделает ваше использование интернета безопасным.</p>
+            </div>
+            <div className={'py-16 text-justify md:text-right'}>
+                <Title className={'py-8'} size={'l'}>Можно использовать на всех устройствах</Title>
+                <p className={'md:text-5xl text-3xl'}>XOMA VPN доступен для всех популярных платформ, включая телефоны, планшеты, компьютеры и другие девайсы.</p>
+            </div>
+        </div>
+        <div className={'bg-black text-white text-center py-20 px-8 w-full flex flex-col justify-center relative items-center gap-4 relative'}>
+            <Title size={"m"}>Готовы попробовать XOMA VPN?</Title>
+            <button
+                className={'text-white border-4 border-white text-xl px-4 py-2 mt-12 hover:bg-white hover:text-black duration-300'}
+                onClick={() => window.open('https://t.me/xomavpn_bot')}>
+                Попробовать
+            </button>
+        </div>
     </div>
 }
 
